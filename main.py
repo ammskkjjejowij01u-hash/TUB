6c8296380babef16294582bb95c1adac48d4f15e
ammskkjjejowij01u-hash <ammskkjjejowij01u@gmail.com>
2026-06-27 21:58:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 from telethon import TelegramClient, events
 
-api_id = 33765228
-api_hash = "d2767c387914ee3b344f2c1367e69a1f"
-string_session = "1BJWap1sBu199Ig5LND4HD5rCrLWoq8j3KSNfLa2WnuO4yLU7gx6RvjwBb_6a3ewdokiirIqdLz9Ulohe2cHmTAo1FwVD3ArAPrQ7629zY9cia2Erd865VdY1C-sm219bludLAHuwrnmEo0FcHdtHX1RjY779UpHUq13RfnUwvwu9aP2d4L-gDn566wuJw79ztbglp7WD8hNRIFl1G655lmfoD3Il28EeOK64ZCpi5dmR2WJhGvsGqYWykWpY3Mc-4-jpdMFoxsEe5pH_kFlRV2e9TotMatboxaZoXeRC20_csFDNM25h9kEPAmZz-nbvhUYBRIQKFuP_LpQaO4THqELAfAR-0Eg="
+api_id = int(os.environ["api_id"])
+api_hash = os.environ["api_hash"]
+string_session = os.environ["string_session"]
 
 client = TelegramClient(string_session, api_id, api_hash)
 
@@ -19,13 +20,7 @@ async def handler(event):
     if text == ".id":
         await event.reply(str(event.chat_id))
 
-    if text == ".help":
-        await event.reply("commands: .سلام .ping .id")
 
-def main():
-    print("Userbot running...")
-    client.start()
-    client.run_until_disconnected()
-
-if __name__ == "__main__":
-    main()
+print("Userbot running...")
+client.start()
+client.run_until_disconnected()
